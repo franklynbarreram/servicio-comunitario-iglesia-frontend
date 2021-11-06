00ed1aa31a9ecf62618a6938bb6a2a4af30f85c7
Franklyn Eduardo Medina <fbarrera@shokworks.io>
2021-11-06 19:50:04 -0400

    cambiando colores y logo

--- consts/images.ts
+++ consts/images.ts
@@ -1,4 +1,6 @@
 export enum Images {
-	logoWhite = '/logos/logo-white.svg',
-	logoWithColor = '/logos/logo.svg',
+	// logoWhite = '/logos/logo-white.svg',
+	// logoWithColor = '/logos/logo.svg',
+	logoWhite = '/logos/logo.png',
+	logoWithColor = '/logos/logo.png',
 }
